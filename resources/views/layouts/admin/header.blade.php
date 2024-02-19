832b0d52849bfb3bf2453c575b9851295a5506d3
BernardoBarbucci <145600436+BernardoBarbucci@users.noreply.github.com>
2024-02-19 16:25:13 +0100

    Initial commit

---
+++ resources/views/layouts/admin/header.blade.php
@@ -0,0 +1,6 @@
+<header class="container-fluid bg-dark">
+    <section class="text-center py-3">
+        <h2 class="fw-bold text-success">Projects Portfolio</h2>
+        <a href="{{ route('admin.data.create') }}" class="btn btn-success me-3">Create</a>
+    </section>
+</header>
