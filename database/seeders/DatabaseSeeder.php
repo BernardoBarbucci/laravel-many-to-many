fcbd9e9467ab8bd7bd19f6e6b5c6a8773e04569f
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-02-19 18:20:39 +0100

    aggiunto li nella show per mostrare le technologies usate nella many-to-many

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ProjectSeeder::class,
+            ProjectSeeder::class,
+            TechnologiesSeeder::class,
+            ProjectTechnologySeeder::class,
         ]);
     }
 }
